6801be8bf674321445d0c2b3d4336ecfbbd0b91f
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-13 13:38:47 -0500

    finish first reusable component

--- src/index.js
+++ src/index.js
@@ -13,7 +13,11 @@ const root = document.getElementById('app');
 //render the app
 render(
 	<>
-		<Badge color='red'>Hey</Badge>
+		<h1>Check out these badges!</h1>
+		<Badge color='green'>Success</Badge> This is operational. <br />
+		<Badge color='red'>Removed</Badge> This is critical. <br />
+		<Badge color='yellow'>Warning</Badge> This is a warning. <br />
+		<Badge color='blue'>Beta</Badge> This is in progress. <br />
 	</>,
 	root,
 );
